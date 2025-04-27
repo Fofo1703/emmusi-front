e21b0107fe904f322606b1c16552eb7e7de63ed1
Fofo1703 <51063226+Fofo1703@users.noreply.github.com>
2025-04-27 00:41:51 -0600

    Navegacion y tiempo de secion

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,54 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from '../pages/login/login';
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "../pages/login/login";
 import FormStudent from "../pages/students/formStudent";
+import ProtectedRoute from "../routes/ProtectedRoute";
 
-function PublicRoutes() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/estudiantes" element={<FormStudent />} />
-            </Routes>
-        </BrowserRouter>
-    );
-}
+const LOGOUT_TIME = 30 * 60 * 1000; // 30 minutos en milisegundos
 
-export default PublicRoutes
+const PublicRoutes = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem("userToken"));
+
+  useEffect(() => {
+    let logoutTimer;
+
+    const resetTimer = () => {
+      clearTimeout(logoutTimer);
+      if (isAuthenticated) {
+        logoutTimer = setTimeout(() => {
+          localStorage.removeItem("userToken");
+          setIsAuthenticated(false); // <-- actualizar estado
+          window.location.href = "/";
+        }, LOGOUT_TIME);
+      }
+    };
+
+    window.addEventListener("mousemove", resetTimer);
+    window.addEventListener("keydown", resetTimer);
+    window.addEventListener("scroll", resetTimer);
+    window.addEventListener("click", resetTimer);
+
+    resetTimer();
+
+    return () => {
+      clearTimeout(logoutTimer);
+      window.removeEventListener("mousemove", resetTimer);
+      window.removeEventListener("keydown", resetTimer);
+      window.removeEventListener("scroll", resetTimer);
+      window.removeEventListener("click", resetTimer);
+    };
+  }, [isAuthenticated]);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+        <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
+          <Route path="/estudiantes" element={<FormStudent />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default PublicRoutes;
